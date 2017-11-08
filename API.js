eafc0c85aa5c0c71da250acbb6f2eabc4b6632ea
Garrett Troy <gt0840922@otc.edu>
2017-11-08 12:17:12 -0600

    Second Commit

--- API.js
+++ API.js
@@ -1,5 +1,25 @@
 // https://api.icndb.com
 
 $(function () {
-    el:'#app'
+    const vm = new Vue({
+        el:'#app',
+        data: {
+            loading: false,
+            showing: 'jokes',
+            jokes: [],
+            joke: null
+        },
+        methods: {
+            loadJokes() {
+                this.loading = true
+
+                this.$http.get('https://api.icndb.com/jokes/random/25')
+                .then(resp => {
+                    this.jokes = resp.body.value
+                    this.loading = false
+                })
+            }
+        }
+    })
+    
 })
